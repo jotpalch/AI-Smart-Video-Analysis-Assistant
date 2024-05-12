d9c30a0cca10c81f051b1c488c47f6b86d69310e
jotpalch <jotp076315217@gmail.com>
2024-05-12 19:55:56 +0800

    feat: add markdown support in chat

--- Front-end/app/components/chat.jsx
+++ Front-end/app/components/chat.jsx
@@ -3,6 +3,8 @@
 import { useState } from "react";
 import { useMsgStore } from "./store";
 
+import TextareaAutosize from 'react-textarea-autosize';
+
 export function Chat() {
 	const [value, setValue] = useState("");
 	const msg = useMsgStore((state) => state.currentSessionIndex);
@@ -19,7 +21,7 @@ export function Chat() {
 	};
 
 	const onKeyDown = (e) => {
-		if (e.key === 'Enter' && !e.shiftKey) {
+		if (e.key === "Enter" && !e.shiftKey) {
 			e.preventDefault();
 			onSubmit(e);
 		}
@@ -28,14 +30,15 @@ export function Chat() {
 	return (
 		<div className="relative">
 			<form onSubmit={onSubmit}>
-				<textarea
-					rows="3"
+				<TextareaAutosize
+					minRows={3}
+					maxRows={20}
 					placeholder="Press Enter to send message and Shift + Enter for new line"
 					value={value}
 					onChange={onChange}
 					onKeyDown={onKeyDown}
 					className="block w-full p-4 ps-5 text-sm text-gray-900 border resize-none border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-				></textarea>
+				></TextareaAutosize>
 				<button
 					type="submit"
 					className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
